95f5ea1a372de651fda82107a1dc428de522de52
Malek Ascha <malekascha@gmail.com>
2016-07-22 21:35:31 -0700

    added port env

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
   // "gopkg.in/mgo.v2"
   "net/http"
+  "os"
 )
 
 func main(){
+
+  port := os.Getenv("PORT")
+
+  if (port == "") {
+    port = ":3000"
+  }
+
   http.HandleFunc("/", helloWorld)
-  http.ListenAndServe(":3000", http.DefaultServeMux)
+  http.ListenAndServe(port, http.DefaultServeMux)
 }
 
